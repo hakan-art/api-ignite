b6c31035f202ff51c52f4e373d0049e021bd5afe
hakan-art <hakan.gezer74@googlemail.com>
2020-12-31 09:49:41 +0100

    9. React Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import GlobalStyles from './components/GlobalStyles';
 import Home from './pages/Home';
 
+import { Route } from "react-router-dom";
 
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Home />
+      <Route path={["/game/:id", "/"]}>
+        <Home />
+      </Route>
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,8 @@ import { motion } from "framer-motion";
 import { useDispatch } from "react-redux";
 import { loadDetail } from "../actions/detailAction";
 
+import { Link } from "react-router-dom";
+
 const Game = ({ name, released, image, id }) => {
     //Load Details
     const dispatch = useDispatch();
@@ -16,11 +18,13 @@ const Game = ({ name, released, image, id }) => {
     return (
 
         <StyledGame onClick={loadDetailHandler}>
-            <h3>
-                {name}
-            </h3>
-            <p>{released} </p>
-            <img src={image} alt={name} />
+            <Link to={`/game/${id}`}>
+                <h3>
+                    {name}
+                </h3>
+                <p>{released} </p>
+                <img src={image} alt={name} />
+            </Link>
         </StyledGame>
     );
 }
@@ -30,6 +34,7 @@ min-height: 30vh;
 box-shadow: 0px 5px 30px rgba(0,0,0,0.2);
 text-align: center;
 border-radius: 1rem;
+cursor: pointer;
 img{
     width: 100%;
     height: 40vh;
